9f75b0bb26302373c878e2d3dfa308692ec0ddd0
hoanthanh <lvhoanthanh@gmail.com>
2025-08-26 05:22:32 +0700

    update code

--- src/problem1/index.js
+++ src/problem1/index.js
@@ -1,10 +1,26 @@
 // Formula
 var sum_to_n_a = function (n) {
+  if (n <= 0) {
+    return 0;
+  }
+
+  if (!Number.isInteger(n)) {
+    return "n is not a integer";
+  }
+  
   return (n * (n + 1)) / 2;
 };
 
 // Loop
 var sum_to_n_b = function (n) {
+  if (n <= 0) {
+    return 0;
+  }
+
+  if (!Number.isInteger(n)) {
+    return "n is not a integer";
+  }
+
   let sum = 0;
   for (let i = 1; i <= n; i++) {
     sum += i;
@@ -14,6 +30,17 @@ var sum_to_n_b = function (n) {
 
 // Recursive
 var sum_to_n_c = function (n) {
-  if (n <= 1) return n;
+  if (n <= 0) {
+    return 0;
+  }
+
+  if (!Number.isInteger(n)) {
+    return "n is not a integer";
+  }
+
+  if (n === 1) {
+    return 1;
+  }
+
   return n + sum_to_n_recursive(n - 1);
 };
--- src/problem4/index.js
+++ src/problem4/index.js
@@ -1,19 +1,47 @@
 // Loop
 function sum_to_n(n) {
-    let sum = 0;
-    for (let i = 1; i <= n; i++) {
-        sum += i;
-    }
-    return sum;
+  if (n <= 0) {
+    return 0;
+  }
+
+  if (!Number.isInteger(n)) {
+    return "n is not a integer";
+  }
+
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+
+  return sum;
 }
 
 // Recursion
 function sum_to_n(n) {
-    if (n <= 0) return 0;
-    return n + sum_to_n(n - 1);
+  if (n <= 0) {
+    return 0;
+  }
+
+  if (!Number.isInteger(n)) {
+    return "n is not a integer";
+  }
+
+  if (n === 1) {
+    return 1;
+  }
+
+  return n + sum_to_n(n - 1);
 }
 
 // Formula
 function sum_to_n(n) {
-    return n * (n + 1) / 2;
+   if (n <= 0) {
+    return 0;
+  }
+
+  if (!Number.isInteger(n)) {
+    return "n is not a integer";
+  }
+  
+  return (n * (n + 1)) / 2;
 }
